148eadca0075488323e2331d19f59512a1e0a24b
rajeshoo7 <S534844@NWMISSOURI.EDU>
2019-07-30 19:36:44 -0500

    Initiated a Sample MVC website and understood parts of it by a Creating a sample MVC for Product

---
+++ MVCStarter/MVCStarter/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using MVCStarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStarter.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ViewBag.TestMessage = "Testing the message from the Product Controller";
+            List<Product> products = new List<Product>();
+            products.Add(new Product() { ProductId = 1, ProductName = "Product 1" });
+            products.Add(new Product() { ProductId = 2, ProductName = "Product 2" });
+            products.Add(new Product() { ProductId = 3, ProductName = "Product 3" });
+
+            return View(products);
+        }
+        public ActionResult PartialProduct()
+        {
+            ViewBag.Message = "Partial Data";
+            return View();
+        }
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+    }
+}

bd6ceb35dda8b3e93d1b5919691f8af252081063
rajeshoo7 <S534844@NWMISSOURI.EDU>
2019-08-03 12:01:01 -0500

    Initiated the eCommerce Website to build using MVC .Net

---
+++ eCommerce/eCommerce.DAL/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        // you can either pass the name of the connection string (e.g. from a web.config), and explicitly declare here.
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+        // Any Entity to be persisted to be declared here
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ eCommerce/eCommerce.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using eCommerce.DAL.Data;
+using eCommerce.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace eCommerce.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CustomerRepository customers = new CustomerRepository(new DataContext());
+            ProductRepository products = new ProductRepository(new DataContext());
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
